74d05f8d88b3e512e0ed5d4ee9773967e6d812ba
Vallel Blanco <vallelblanco@gmail.com>
2017-05-16 20:58:25 -0700

    Created template with bootstrap

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Twitch Chat Bot', indexPage: true });
+});
+
+/* GET song request page */
+router.get('/songrequest', function(req, res, next) {
+  res.render('songrequest', { title: 'Twitch Chat Bot - Song Request', songRequestPage: true });
 });
 
 module.exports = router;
